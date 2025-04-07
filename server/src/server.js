1964f258fddb331fe35a490520eb92fbfaafbbcb
m-h-thomas <mhthomas.8193@gmail.com>
2025-04-07 10:30:37 -0600

    added boiler plate Express server setup

---
+++ server/src/server.js
@@ -0,0 +1,10 @@
+//Boiler plate setup
+const express = require('express');
+const app = express();
+const port = 8080;
+
+//Basic home route - UPDATE LATER
+app.get('/', (req, res) => res.send('Hello World!'))
+
+//Console message confirming Express is up and running.
+app.listen(port, () => console.log(`Express servers is listening at http://localhost:${port}`))
