de9f48fb079cb3b6e823294e837cd410a167d288
Chaoss66-dev <erik.voss@spaceforce.mil>
2025-04-07 09:48:02 -0700

    adding default testing infrastructure

--- server/src/server.js
+++ server/src/server.js
@@ -3,8 +3,12 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.use(express.json())
+
 //Basic home route - UPDATE LATER
 app.get('/', (req, res) => res.send('Hello World!'))
 
 //Console message confirming Express is up and running.
 app.listen(port, () => console.log(`Express servers is listening at http://localhost:${port}`))
+
+module.exports = app
--- server/src/tests/default.test.js
+++ server/src/tests/default.test.js
@@ -1,2 +1,10 @@
 const request = require('supertest')
-const server = require('../server.js')
+const app = require('../server.js')
+
+describe('GET /', () => {
+    it('should return a default message', async () => {
+      const response = await request(app).get('/');
+      expect(response.status).toBe(200);
+      expect(response.text).toBe('Hello World!')
+    });
+  });
