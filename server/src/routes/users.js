a41c79cfce316b5bba48bb53e706b008c109b0c9
Tollhouse <mistertollhouse@gmail.com>
2025-04-07 12:24:54 -0600

    Started adding in the initial startup for express/cors

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,5 +1,13 @@
+const cors = require('cors');
 const express = require("express");
 const router = express.Router();
 const knex = require("knex")(require("../../knexfile")["development"]);
+
+router.use(express.json())
+router.use(cors())
+
+router.get('/users', async (req, res) => {
+    res.status(200).json({message:"Working route."})
+})
 
 module.exports = router;
