fdf1483e17aedeec276b6eaf9edc287278cd6fba
Dytzi <64977626+Dytzi@users.noreply.github.com>
2025-04-08 13:41:33 -0600

    added get route users/schedule and users/schedule/:id to get user schedules

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -16,6 +16,66 @@ router.get('/', async (req, res) => {
     res.status(200).json({message:"Working route."})
 });
 
+
+// get route to get the schedule of every user
+router.get("/schedule", async (req, res) => {
+    let data = []
+
+    try{
+        let courseDates = await knex("users")
+        .leftJoin('course_registration', 'users.id', 'user_id')
+        .leftJoin('courses', 'courses.id', 'course_id')
+        .select('users.id as id','course_id',  'date_start', 'date_end')
+        .select(knex.raw(`'courses' as source`));
+
+        let crewDates = await knex("users")
+        .leftJoin('crews', 'users.crew_id', 'crews.id')
+        .leftJoin('crew_rotations', 'crews.id', 'crew_rotations.crew_id')
+        .select('users.id as id', 'date_start', 'date_end')
+        .select(knex.raw(`'crews' as source`));
+
+        data.push({crewDates})
+        data.push({courseDates})
+
+        return res.status(200).json(data)
+    }
+    catch (error){
+        return res.status(500).json({ error: error });
+    }
+
+});
+
+// get route to get the schedule of every user
+router.get("/schedule/:id", async (req, res) => {
+    const id = parseInt(req.params.id)
+    let data = []
+
+    try{
+        let courseDates = await knex("users")
+        .leftJoin('course_registration', 'users.id', 'user_id')
+        .leftJoin('courses', 'courses.id', 'course_id')
+        .select('users.id as id','course_id',  'date_start', 'date_end')
+        .select(knex.raw(`'courses' as source`))
+        .where('users.id', id);
+
+        let crewDates = await knex("users")
+        .leftJoin('crews', 'users.crew_id', 'crews.id')
+        .leftJoin('crew_rotations', 'crews.id', 'crew_rotations.crew_id')
+        .select('users.id as id', 'date_start', 'date_end')
+        .select(knex.raw(`'crews' as source`))
+        .where('users.id', id);
+
+        data.push({crewDates})
+        data.push({courseDates})
+
+        return res.status(200).json(data)
+    }
+    catch (error){
+        return res.status(500).json({ error: error });
+    }
+
+});
+
 router.get("/:id", async (req, res) => {
     const id = parseInt(req.params.id)
     if(typeof id !== "number" || isNaN(id)){
