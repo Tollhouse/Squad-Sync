5d635f351a1df3f7931eb4c6c68d8e3b4ce6d61c
lorenalongoria <lorena.longoria.1@spaceforce.mil>
2025-04-08 12:22:28 -0400

    footer compatible w/ light/dark modes

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -9,30 +9,30 @@ import './Footer.css';
 function Footer() {
   const currentYear = new Date().getFullYear();
 
-return (
-  <Box
-    component="footer"
-    sx={{
-      position: "fixed",
-      bottom: 0,
-      left: 0,
-      width: "100%",
-      bgcolor: "transparent",
-      color: "white",
-      py: 2,
-      textAlign: "center",
-      zIndex: 1000,
-    }}
-  >
-    <Container>
-      <Typography variant="body2">
-        &copy; {currentYear} Curtis Bonham, Lorena Longoria, Erik Voss,
-        Essence Jackson, Jackie Luu, Landon Spencer, Michael Thomas, Tyson
-        Butler-Currier, and Harman Gidda. All rights reserved.
-      </Typography>
-    </Container>
-  </Box>
-);
+  return (
+    <Box
+      component="footer"
+      sx={(theme) => ({
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        width: "100%",
+        bgcolor: "transparent",
+        color: theme.palette.text.primary,
+        py: 2,
+        textAlign: "center",
+        zIndex: 1000,
+      })}
+    >
+      <Container>
+        <Typography variant="body2">
+          &copy; {currentYear} Curtis Bonham, Tyson
+          Butler-Currier, Harman Gidda, Essence Jackson, Lorena Longoria, Jackie Luu, Landon Spencer,
+          Michael Thomas, and Erik Voss. All rights reserved.
+        </Typography>
+      </Container>
+    </Box>
+  );
 }
 
 export default Footer;
