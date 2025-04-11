99cab753bccadde0db9a105389cfde662726c795
Landon-Spencer <landon.r.spencer@gmail.com>
2025-04-11 12:02:58 -0600

    updated default new user and added seats and descritption to course

--- server/seeds/03_crew_rotations.js
+++ server/seeds/03_crew_rotations.js
@@ -8,11 +8,11 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('crew_rotations').del()
   await knex('crew_rotations').insert([
-    {crew_id: 1, date_start: '05-01-2025', date_end: '05-04-2025', shift_type: 'day', shift_duration: 8, experience_type: 'green'},
-    {crew_id: 2, date_start: '05-01-2025', date_end: '05-04-2025', shift_type: 'swing', shift_duration: 8, experience_type: 'yellow'},
-    {crew_id: 3, date_start: '05-01-2025', date_end: '05-04-2025', shift_type: 'night', shift_duration: 8, experience_type: 'red'},
-    {crew_id: 4, date_start: '05-01-2025', date_end: '05-04-2025', shift_type: 'rest', shift_duration: 8, experience_type: 'green'},
-    {crew_id: 5, date_start: '05-01-2025', date_end: '05-04-2025', shift_type: 'rest', shift_duration: 8, experience_type: 'green'},
-    {crew_id: 6, date_start: '05-01-2025', date_end: '05-04-2025', shift_type: 'rest', shift_duration: 8, experience_type: 'green'},
+    {crew_id: 1, date_start: '05-01-2025', date_end: '05-04-2025', shift_type: 'Day', shift_duration: 8, experience_type: 'green'},
+    {crew_id: 2, date_start: '05-01-2025', date_end: '05-04-2025', shift_type: 'Swing', shift_duration: 8, experience_type: 'yellow'},
+    {crew_id: 3, date_start: '05-01-2025', date_end: '05-04-2025', shift_type: 'Night', shift_duration: 8, experience_type: 'red'},
+    {crew_id: 4, date_start: '05-01-2025', date_end: '05-04-2025', shift_type: 'Day', shift_duration: 12, experience_type: 'green'},
+    {crew_id: 5, date_start: '05-01-2025', date_end: '05-04-2025', shift_type: 'Night', shift_duration: 12, experience_type: 'green'},
+    {crew_id: 6, date_start: '05-01-2025', date_end: '05-04-2025', shift_type: 'Rest', shift_duration: 8, experience_type: 'green'},
   ]);
 };
